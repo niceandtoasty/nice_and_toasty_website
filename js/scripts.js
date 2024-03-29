9095d7b2b92e02e978ddbca71c6ea20dd8af5605
tkmcnierney <tkmcnierney@berkeley.edu>
2024-03-29 16:00:13 -0700

    add nice and toasty landing page

---
+++ js/scripts.js
@@ -0,0 +1,60 @@
+/*!
+* Start Bootstrap - New Age v6.0.7 (https://startbootstrap.com/theme/new-age)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-new-age/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+// Summon alert dialog when form is submitted
+window.addEventListener("load", function() {
+    const form = document.getElementById('email-form');
+    const btn = document.getElementById('submit-button');
+    form.addEventListener("submit", function(e) {
+      e.preventDefault();
+
+      btn.disabled = true;
+      btn.innerText = "Submitting...";
+      const data = new FormData(form);
+      const action = e.target.action;
+      fetch(action, {
+        method: 'POST',
+        body: data,
+      })
+      .then(() => {
+        form.reset();
+        btn.outerHTML = "<div class='text-center btn-div mb-3'><div class='fw-bolder'>Thanks for submitting!</div></div>";
+      })
+      .catch(error => {
+        form.reset();
+        btn.outerHTML = "<div class='text-center text-danger mb-3'>There was an error</div>";
+      })
+    });
+  });
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
